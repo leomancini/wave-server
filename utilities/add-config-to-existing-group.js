f671cb8b171ef5152bfbfa62b6c8947001f835ec
Leo Mancini <mancini@leo.gd>
2024-12-30 12:38:49 +0800

    Adding error state

--- utilities/add-config-to-existing-group.js
+++ utilities/add-config-to-existing-group.js
@@ -2,26 +2,35 @@ import fs from "fs";
 import path from "path";
 
 export default (groupId) => {
-  const groupPath = `groups/${groupId}`;
-  const mediaPath = `${groupPath}/media`;
-  const mediaFiles = fs.readdirSync(mediaPath);
+  try {
+    const groupPath = `groups/${groupId}`;
+    const mediaPath = `${groupPath}/media`;
+    const mediaFiles = fs.readdirSync(mediaPath);
 
-  const earliestDate = mediaFiles.reduce((earliest, file) => {
-    const stats = fs.statSync(path.join(mediaPath, file));
-    return earliest ? Math.min(earliest, stats.birthtimeMs) : stats.birthtimeMs;
-  }, null);
+    const earliestDate = mediaFiles.reduce((earliest, file) => {
+      const stats = fs.statSync(path.join(mediaPath, file));
+      return earliest
+        ? Math.min(earliest, stats.birthtimeMs)
+        : stats.birthtimeMs;
+    }, null);
 
-  const config = {
-    createdAt: new Date(earliestDate).toISOString(),
-    reactions: ["👍", "👎", "🤣", "🤔", "🤮"]
-  };
+    const config = {
+      createdAt: new Date(earliestDate).toISOString(),
+      reactions: ["👍", "👎", "🤣", "🤔", "🤮"]
+    };
 
-  fs.writeFileSync(
-    path.join(groupPath, "config.json"),
-    JSON.stringify(config, null, 2)
-  );
+    fs.writeFileSync(
+      path.join(groupPath, "config.json"),
+      JSON.stringify(config, null, 2)
+    );
 
-  return {
-    message: `Successfully added config to group ${groupId}`
-  };
+    return {
+      message: `Successfully added config to group ${groupId}`
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      error: "Failed to add config to group"
+    };
+  }
 };
